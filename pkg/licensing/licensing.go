d0aaacc4dc2b87bc0e4d182e6f60e7c0dbef7304
James Washington <jwashington104.jw@gmail.com>
2023-05-15 10:19:35 +0100

    Updated license struct

--- pkg/licensing/licensing.go
+++ pkg/licensing/licensing.go
@@ -3,11 +3,11 @@ package licensing
 import "time"
 
 type License struct {
-	Issuer           string
-	ValidFrom        time.Time
-	ValidUntil       time.Time
-	ExpiryDate       time.Time
-	ValidityPeriod   int
-	OrganizationName string
-	LicenseID        string
+	Issuer           string    `json:"iss"`
+	IssuedAt         time.Time `json:"iat"`
+	ValidFrom        time.Time `json:"nbf"`
+	ValidUntil       time.Time `json:"exp"`
+	ValidityPeriod   int       `json:"validity_period"`
+	OrganizationName string    `json:"org_name"`
+	LicenseID        string    `json:"license_id"`
 }
