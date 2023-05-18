e6bb8674c5ffce3a84a4ebb49ffeb07a139d24ef
James Washington <jwashington104.jw@gmail.com>
2023-05-18 21:02:14 +0100

    Added basic database packages

---
+++ pkg/db_connector/db_connector.go
@@ -0,0 +1,53 @@
+package dbconnector
+
+import (
+	"errors"
+	"os"
+)
+
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+}
+
+func GetDBConfigFromEnv() (*DBConfig, error) {
+	Host := os.Getenv("DB_HOST")
+	if Host == "" {
+		return nil, errors.New("DB_HOST not set")
+	}
+
+	Port := os.Getenv("DB_PORT")
+	if Port == "" {
+		return nil, errors.New("DB_PORT not set")
+	}
+
+	User := os.Getenv("DB_USER")
+	if User == "" {
+		return nil, errors.New("DB_USER not set")
+	}
+
+	Password := os.Getenv("DB_PASSWORD")
+	if Password == "" {
+		return nil, errors.New("DB_PASSWORD not set")
+	}
+
+	Database := os.Getenv("DB_DATABASE")
+	if Database == "" {
+		return nil, errors.New("DB_DATABASE not set")
+	}
+
+	return GetDBConfig(Host, Port, User, Password, Database), nil
+}
+
+func GetDBConfig(Host, Port, User, Password, Database string) *DBConfig {
+	return &DBConfig{
+		Host:     Host,
+		Port:     Port,
+		User:     User,
+		Password: Password,
+		Database: Database,
+	}
+}
